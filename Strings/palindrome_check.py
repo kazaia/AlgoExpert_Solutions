1340b4a173b961a5e1346c1c5cbd7ebe8c911fbc
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-16 07:36:43 +0100

    Create palindrome_check.py

---
+++ Strings/palindrome_check.py
@@ -0,0 +1,44 @@
+# Palindrome check
+
+# Solution 1:
+def isPalindrome(string):
+     return string == string[::-1]
+
+
+# Solution 2;
+def isPalindrome(string):
+    reversed = []
+    lst = [x for x in string]
+    for i in range(len(lst) -1, -1, -1):
+        reversed.append(lst[i])
+    return reversed == lst
+
+
+# Solution 3:
+# Time complexity: O(n^2). In python, strings are immutable, each time we create a new string which take time.
+# Space complexity: new strings creation
+def isPalindrome(string):
+    reversedString = ""
+    for i in reversed(range(len(string))):
+        reversedString += string[i]
+    return string == reversedString
+
+
+# Solution 4:
+# Time complexity: O(n) Instead of creating new strings each time, we use a list wich is ,unlike strings, mutable.
+# Space complexity: O(n) as we creted a list of lenght n. 
+def isPalindrome(string):
+    reversedString = []
+    for i in reversed(range(len(string))):
+        reversedString.append(string[i])
+    return string == "".join(reversedString)
+
+# Solution 5: Recursive approach 
+
+
+
+
+
+
+
+# Solution6: two pointers approach
