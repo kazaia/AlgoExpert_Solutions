465b9eabd4074224a5bc6893cb7b857b3f4c9bae
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-16 09:43:14 +0100

    Update Nth_Fibonacci.py

--- Recursion/Nth_Fibonacci.py
+++ Recursion/Nth_Fibonacci.py
@@ -1,12 +1,12 @@
 # Nth fibonacci
-
+# Time complexity without memoization converge to 2^n, because for each given n we call 2 function fib(n-1) and fib(n-2) and each function calls 2 fuctions and so on and so forth
 
 cache = {}
-def getNthFib(self, n):
+def fib(self, n):
     if n in cache:
         return cache[n]
     else:
 	if n == 0 : return 0
 	if n == 1 or n == 2: return 1
-	cache[n] = getNthFib(n - 2) + getNthFib(n - 1) 
+	cache[n] = fib(n - 2) + fib(n - 1) 
 	return 	cache[n] 
