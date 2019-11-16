9497db90d1e9d1db518717582220727851d4cccf
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-16 06:57:29 +0100

    Update palindrome_check.py

--- palindrome_check.py
+++ palindrome_check.py
@@ -12,3 +12,13 @@ def isPalindrome(string):
     for i in range(len(lst) -1, -1, -1):
         reversed.append(lst[i])
     return reversed == lst
+
+
+# Solution 3:
+# Time complexity: O(n^2). In python, strings are immutable, each time we create a new string which take time.
+# Space complexity: new strings creation
+def isPalindrome(string):
+    reversedString = ""
+    for i in reversed(range(len(string))):
+        reversedString += string[i]
+    return string == reversedString
