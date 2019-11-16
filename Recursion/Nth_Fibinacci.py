64f11ee837f12b953f1a9fae4f8e969e2af414a0
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-16 09:23:15 +0100

    Update and rename Nth_Fibinacci.py to Nth_Fibonacci.py

--- Recursion/Nth_Fibinacci.py
+++
@@ -1,4 +0,0 @@
-# Nth fibonacci
-
-
-
---
+++ Recursion/Nth_Fibonacci.py
@@ -0,0 +1,12 @@
+# Nth fibonacci
+
+
+cache = {}
+def getNthFib(self, n):
+    if n in cache:
+        return cache[n]
+    else:
+		if n == 0 : return 0
+		if n == 1 or n == 2: return 1
+		cache[n] = getNthFib(n - 2) + getNthFib(n - 1) 
+		return 	cache[n] 
