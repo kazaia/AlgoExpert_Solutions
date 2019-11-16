57be00c2306094df36e8a6378ab495e4c7c74c33
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-16 09:23:38 +0100

    Update Nth_Fibonacci.py

--- Recursion/Nth_Fibonacci.py
+++ Recursion/Nth_Fibonacci.py
@@ -6,7 +6,7 @@ def getNthFib(self, n):
     if n in cache:
         return cache[n]
     else:
-		if n == 0 : return 0
-		if n == 1 or n == 2: return 1
-		cache[n] = getNthFib(n - 2) + getNthFib(n - 1) 
-		return 	cache[n] 
+	if n == 0 : return 0
+	if n == 1 or n == 2: return 1
+	cache[n] = getNthFib(n - 2) + getNthFib(n - 1) 
+	return 	cache[n] 
