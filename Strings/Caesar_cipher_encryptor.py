304dc9b1e6e80ba8f901bb14087695abd08d24ef
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-17 19:12:24 +0100

    Update Caesar_cipher_encryptor.py

--- Strings/Caesar_cipher_encryptor.py
+++ Strings/Caesar_cipher_encryptor.py
@@ -1,10 +1,17 @@
 # Caesar cipher encryptor
 
 def caesarCipherEncryptor(string, key):
+	key = key % 26
     lst = list(string)
     unicode_plus_key, output = [], []
 	
     for i, s in enumerate(string):
-        unicode_plus_key.append(ord(s)+key % 26)
+		new_code = ord(s)+key
+		if new_code < 123:
+			unicode_plus_key.append(new_code)
+		else:
+			unicode_plus_key.append(96 + new_code % 122)
         output.append(chr(unicode_plus_key[i]))
     return ''.join(output)	
+	
+	
