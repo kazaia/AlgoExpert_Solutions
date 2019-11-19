e57beac155db0e62e698e0b753614d835f67e0ee
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-19 22:23:05 +0100

    Update two_number_sum.py

--- Arrays/two_number_sum.py
+++ Arrays/two_number_sum.py
@@ -1,2 +1,26 @@
 # Two number sum 
 
+# Solution1: Brute force 
+
+
+
+# Solution2: with hash table
+
+
+
+# Solution3: with two pointers 
+
+
+def twoNumberSum(array, targetSum):
+	array.sort()
+	i = 0
+	j = len(array) - 1
+	while i < j:
+		if array[i] + array[j] > targetSum:
+			j -= 1
+		elif array[i] + array[j] < targetSum:
+			i += 1
+		elif array[i] + array[j] == targetSum:
+			return [array[i], array[j]]
+		else:
+			return [ ]
