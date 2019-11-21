40cad62cf04fa765a00a9bdc90652271682bb0a9
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-21 21:49:01 +0100

    Update Binary_search.py

--- Searching/Binary_search.py
+++ Searching/Binary_search.py
@@ -1,6 +1,6 @@
-# Binary search
-# Time complexity: o(n)
-# Space complexity: O(n)
+# Binary search: with recursion
+# Time complexity: o(log(n))
+# Space complexity: O(log(n))
 
 def binarySearch(array, target):
     return binarySearchHelper(array, target, 0, len(array)-1)
@@ -8,7 +8,6 @@ def binarySearch(array, target):
 def binarySearchHelper(array, target, left, right):
 	if left > right: 
 		return -1 
-	
 	middle = (left + right)//2
 	if array[middle] == target:
 		return middle
@@ -16,3 +15,22 @@ def binarySearchHelper(array, target, left, right):
 		return binarySearchHelper(array, target, left, middle -1)
 	else:
 		return binarySearchHelper(array, target, middle +1, right)
+
+	
+#Iterative solution:
+#Time complexity: O(log(n))
+#Space complexity: O(1)
+
+def binarySearch(array, target):
+    left = 0
+    right = len(array) -1
+    while left <= right: 
+	middle = (left + right)//2
+	if array[middle] == target:
+	     return middle
+	elif array[middle] > target:
+	     right = middle -1
+	else:
+	     left = middle + 1
+    return -1		
+
