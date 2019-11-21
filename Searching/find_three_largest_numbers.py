204f71db50e11fc33efbf4b0212145a82bb33f06
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-21 23:10:16 +0100

    Update find_three_largest_numbers.py

--- Searching/find_three_largest_numbers.py
+++ Searching/find_three_largest_numbers.py
@@ -1,4 +1,21 @@
 # Find three largest numbers
 
+# O(n) time 
+# O(n) space
 
+def findThreeLargestNumbers(array):
+    output = []
+    i = 0
+    while i < 3:
+        max_num = 0
+        for item in array:
+            if item > max_num:
+                max_num = item
+        output.append(max_num)
+        array.remove(max_num)
+        i += 1
+    output.sort()
+    return output
 
+a = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+print(findThreeLargestNumbers(a))
