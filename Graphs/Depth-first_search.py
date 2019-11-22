a100cc5303aa37aa7a8d0b149ec349cebbcc5d3f
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-22 23:20:08 +0100

    Create Depth-first_search.py

---
+++ Graphs/Depth-first_search.py
@@ -0,0 +1,18 @@
+# Depth first search 
+
+# Do not edit the class below except
+# for the depthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        pass
