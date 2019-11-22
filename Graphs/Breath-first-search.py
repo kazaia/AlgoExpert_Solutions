15c870094d27fcac84c969f09b40ef9e968113ca
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-23 00:24:21 +0100

    Create Breath-first-search.py

---
+++ Graphs/Breath-first-search.py
@@ -0,0 +1,18 @@
+# Breath first check 
+
+# Do not edit the class below except
+# for the breadthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        pass
