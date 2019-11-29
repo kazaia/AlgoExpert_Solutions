3dc1fbd00da6ef1349e47f003eacc607c5c4b278
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-29 22:33:09 +0100

    Update reverse_linked-list.py

--- Linked List/reverse_linked-list.py
+++ Linked List/reverse_linked-list.py
@@ -4,4 +4,13 @@
 #Space complexity: O(1)
 
 
+def reverseLinkedList(head):
+    p1 = None
+    p2 = head
+    while p2:
+        p3 = p2.next
+        p2.next = p1
+        p1 = p2
+        p2 = p3
+    return p1    
 
