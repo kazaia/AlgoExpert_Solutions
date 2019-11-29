8f5bbcf7f7db0c53c0ddff7d2af5a9f60bcab26f
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-29 22:53:10 +0100

    Update balanced_brackets.py

--- Stacks/balanced_brackets.py
+++ Stacks/balanced_brackets.py
@@ -1,2 +1,20 @@
 # Balanced brackets 
 
+def is_match(a, b):
+	if a == "(" and b == ")" or a == "{" and b == "}" or a == "[" and b == "]":
+		return True
+	return False
+
+def balancedBrackets(string):
+	stack = []
+	for c in string:
+		if c == "(" or c == "{" or c == "[":
+			stack.append(c)
+		elif len(stack) != 0:
+			if is_match(stack[-1], c):
+				stack.pop()
+			elif not is_match(stack[-1], c):
+				return False
+		elif len(stack) == 0:
+			return False
+	return len(stack) == 0
