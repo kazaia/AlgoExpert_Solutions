5ee9e73c470cf9b4ae134a31afa15ca3d1699c15
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-11-30 22:10:29 +0100

    Update search_in_sorted_matrix.py

--- Searching/search_in_sorted_matrix.py
+++ Searching/search_in_sorted_matrix.py
@@ -4,14 +4,16 @@
 # Space complexity: O(1)
 
 def searchInSortedMatrix(matrix, target):
+    if not matrix:
+        return [-1, -1]
     row = 0
     col = len(matrix[0]) -1
     while row < len(matrix) and col >= 0:
         if matrix[row][col] > target:
             col -= 1
-            elif  matrix[row][col] < target:
-                row += 1
-            else:
-                return [row, col]
-     return [-1, -1]   
+        elif  matrix[row][col] < target:
+            row += 1
+        else:
+            return [row, col]
+    return [-1, -1]   
   
