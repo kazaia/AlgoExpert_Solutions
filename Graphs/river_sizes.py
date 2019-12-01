d229b41629f309606a3c922fa4c8b1c5ad1ad1ea
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-01 20:19:23 +0100

    Update river_sizes.py

--- Graphs/river_sizes.py
+++ Graphs/river_sizes.py
@@ -1,6 +1,9 @@
 # River sizes
 
-# Time complexity: O(n) where n is the number of all elemnts in the matrix Or O(w*h), where h is the height of the matrix and w is the width of the matrix
+# Time complexity: O(n) where n is the number of all elemnts in the matrix 
+# OR:
+# O(w*h), where h is the height of the matrix and w is the width of the matrix
+
 # Space complexity: O(1) 
 
 
