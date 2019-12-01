0ff7146aafd04e4722b8d3c4b1f92052ff6eda91
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-01 09:38:25 +0100

    Create search_for_range.py

---
+++ Searching/search_for_range.py
@@ -0,0 +1,14 @@
+# Search for range 
+
+#Recursive solution:
+#Time complexity: O(log(n))
+#Space complexity: O(log(n)) , due to the recursive calls 
+
+
+
+
+
+
+#Iterative solution:
+#Time complexity: O(log(n))
+#Space complexity: O(1)
