b162c341bec8dedcc3383091fdb2ba65b5cd9eeb
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-02 22:24:08 +0100

    Update find_closest_value_in_bst.py

--- Binary Search Tree/find_closest_value_in_bst.py
+++ Binary Search Tree/find_closest_value_in_bst.py
@@ -1,2 +1,28 @@
 # Find closest value in BST
 
+# Recursif approach: 
+# Time complexity: 
+#   -> Average case: O(log(n)), where n is the number of nodes (Beacause we are exploring either the right or the left side of the tree)
+#   -> Worst case: O(n), where n is the number of nodes | when we only have one branch
+# Space complexity: 
+#   -> Average case: O(log(n)), where n is the number of nodes (Due to recursion on Python)
+#   -> Worst case: O(n), where n is the number of nodes | when we only have one branch
+
+
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree, target, float("inf"))
+
+def findClosestValueInBstHelper(tree, target, closest):
+    if not tree:
+        return closest
+    if abs(target - closest) > abs(target - tree.value):
+        closest = tree.value
+    if tree.value > target:
+        return findClosestValueInBstHelper(tree.left, target, closest)
+    elif tree.value < target:
+        return findClosestValueInBstHelper(tree.right, target, closest)
+    elif tree.value == target:
+        return tree.value
+    
+        
+
