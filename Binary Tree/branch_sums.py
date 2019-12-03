05bbbf1f3c978abc5092e1e9b0cca919a499dd9b
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-03 20:58:51 +0100

    Update branch_sums.py

--- Binary Tree/branch_sums.py
+++ Binary Tree/branch_sums.py
@@ -5,3 +5,27 @@
 
 
 
+# This is the class of the input root. Do not edit it.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def branchSums(root):
+    sums = []
+    computeBranchSums(root, 0, sums)
+    return sums
+
+def computeBranchSums(root, runningSum, sums):
+    if not root:
+        return []
+		
+	newRunningSum = runningSum + root.value
+
+    if not root.left and not root.right:
+        sums.append(newRunningSum)
+        return
+
+    computeBranchSums(root.left, newRunningSum, sums)
+    computeBranchSums(root.right, newRunningSum, sums)
