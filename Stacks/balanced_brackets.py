05d0bfa714f4aa5887985045caedd10c2918c6ba
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-07 00:37:27 +0100

    Update balanced_brackets.py

--- Stacks/balanced_brackets.py
+++ Stacks/balanced_brackets.py
@@ -36,7 +36,7 @@ def balancedBrackets(string):
         if char in openingBrackets:
             stack.append(char)
         elif char in closingBrackets:
-            if len(stack) == ):
+            if len(stack) == 0:
                 return False
             if stack[-1] == matchingBrackets[char]:
                 stack.pop()
