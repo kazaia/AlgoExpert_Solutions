23a06984903e70a1b0c6658335212c298d644c96
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-07 13:35:25 +0100

    Update Three_number_sum.py

--- Arrays/Three_number_sum.py
+++ Arrays/Three_number_sum.py
@@ -1,3 +1,21 @@
 # Three number sum 
 
+def threeNumberSum(array, targetSum):
+    array.sort()
+    triplet = []
+    for i in range(len(array) -2):
+        left = i + 1
+        right = len(array) -1
+        while left < right:
+            currentSum = array[i] + array[left] + array[right]
+			if currentSum == targetSum:
+                triplet.append([array[i], array[left], array[right]])
+                left += 1
+                right -=1  
+            elif currentSum < targetSum:
+                left += 1     
+			elif currentSum > targetSum:
+                right -= 1	
+    return triplet  
+
 
