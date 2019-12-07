47ef70583f8da605417720a7e95a8afdedb9f7a5
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-07 13:39:54 +0100

    Update Three_number_sum.py

--- Arrays/Three_number_sum.py
+++ Arrays/Three_number_sum.py
@@ -1,21 +1,26 @@
 # Three number sum 
+# Time complexity: O(n^2), because O(n^2) + O(nlog(n)) ~ O(n^2)
+# Space ecomplexity: O(n): due to the extra triplets we created, in the worst case we could go store evry single element
 
-def threeNumberSum(array, targetSum):
-    array.sort()
-    triplet = []
-    for i in range(len(array) -2):
+# Caveat: The given array is non-empty array with distincts elements 
+
+def threeSum(nums):
+    nums.sort()
+    triplet = [] 
+    for i in range(len(nums) -2):
         left = i + 1
-        right = len(array) -1
+        right = len(nums) -1
+
         while left < right:
-            currentSum = array[i] + array[left] + array[right]
-			if currentSum == targetSum:
-                triplet.append([array[i], array[left], array[right]])
+            currentSum = nums[i] + nums[left] + nums[right]
+            if currentSum > 0:
+                right -= 1
+            elif currentSum < 0:
                 left += 1
-                right -=1  
-            elif currentSum < targetSum:
-                left += 1     
-			elif currentSum > targetSum:
-                right -= 1	
-    return triplet  
+            elif currentSum == 0:
+                triplet.append([nums[i], nums[left], nums[right]])
+                left += 1
+                right -=1            
+    return triplet
 
 
