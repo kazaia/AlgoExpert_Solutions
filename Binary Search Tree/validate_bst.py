b0a76a8e7af3222477c7a2403509d1b05f6442a6
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-12 20:13:19 +0100

    Update validate_bst.py

--- Binary Search Tree/validate_bst.py
+++ Binary Search Tree/validate_bst.py
@@ -2,5 +2,23 @@
 
 
 # Time complexity: O(n) where n is the number of nodes 
-# Space complexity: O(n), Due to recursion in Python 
+# Space complexity: ?? 
 
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def validateBst(tree):
+	return validateBstHelper(tree, float("-inf"), float("inf"))
+
+def validateBstHelper(tree, minVal, maxVal):
+	if not tree:
+		return True 
+	if tree.value < minVal or tree.value >= maxVal:
+		return False
+	leftIsValid = validateBstHelper(tree.left, minVal, tree.value)
+	rightIsValid = validateBstHelper(tree.right, tree.value, maxVal)
+	return leftIsValid and rightIsValid
