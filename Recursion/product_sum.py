58d5234bc11f521ec4e5632a4f1cd145fe8424c3
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-14 12:57:41 +0100

    Update product_sum.py

--- Recursion/product_sum.py
+++ Recursion/product_sum.py
@@ -1,5 +1,6 @@
 # Product sum 
-
+# Time complexity: O(n) where n is the number of elements in the array
+# Space complexity: O(n). due to recursion  
 
 def productSum(array, depth = 1):
 	outputSum = 0 
