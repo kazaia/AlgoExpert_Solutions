a0fc3e78b787a6f42c9ef4e972b18c196e4518f6
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-21 13:32:52 +0100

    Update Remove_Nth_node_from_ebd.py

--- Linked List/Remove_Nth_node_from_ebd.py
+++ Linked List/Remove_Nth_node_from_ebd.py
@@ -4,4 +4,27 @@
 # Time comlexity: O(n)
 # Space complexity: O(1)
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+def removeNthFromEnd(head, k):
+    counter = 1
+    first = head
+    second = head
+    while counter <= k:
+        second = second.next
+        counter += 1
+    if second is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return 
+    while second.next is not None: 
+        second = second.next 
+        first = first.next
+    first.next = first.next.next
+    return head
+        
+        
