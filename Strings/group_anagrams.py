c30da535a80be0508c3397c50a8000979c4911d7
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-21 23:38:25 +0100

    Update group_anagrams.py

--- Strings/group_anagrams.py
+++ Strings/group_anagrams.py
@@ -1,6 +1,6 @@
 # Group anagrams 
 
 
-# Time complexity: 
-# Space complexity:
+# Time complexity: O(wnlog(w)), whre n is the lenght of the words and n is the lenght of the array
+# Space complexity: O(wn), whre n is the lenght of the words and n is the lenght of the array
 
