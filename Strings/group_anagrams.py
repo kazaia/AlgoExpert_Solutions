45888ccb2253669869c82788ae76f47a6d37b432
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-21 23:50:16 +0100

    Update group_anagrams.py

--- Strings/group_anagrams.py
+++ Strings/group_anagrams.py
@@ -4,3 +4,17 @@
 # Time complexity: O(wnlog(w)), whre n is the lenght of the words and n is the lenght of the array
 # Space complexity: O(wn), whre n is the lenght of the words and n is the lenght of the array
 
+def groupAnagrams(words):
+    anagrams = {}
+    sortedWords = []
+    for word in words:
+        sortedWord = ''.join(sorted(word))
+        if sortedWord in anagrams:
+            anagrams[sortedWord].append(word)
+        else:
+            anagrams[sortedWord] = [word]
+        
+    return list(anagrams.values())
+
+
+
