7a542ef4ecd16a6b9fe1a681a52e1082e51e0a47
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-30 16:33:08 +0100

    Update smallest_difference.py

--- Arrays/smallest_difference.py
+++ Arrays/smallest_difference.py
@@ -1,4 +1,4 @@
 # Smallest difference 
 
 # Time complexity: O(nlog(n) + mlog(m)), where n is the lenght of the first array and m is the lenght of the second array
-# Space complexity: 
+# Space complexity: O(1)
