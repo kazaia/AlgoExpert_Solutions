472fbbf2df701c26a40a583f61fbb30cfee34923
kaoutar ZAIA <kaoutarZaia@gmail.com>
2019-12-31 12:44:40 +0100

    Update Maximum_subset_Sum_with_No_adjacent_Elements.py

--- Dynamic programming/Maximum_subset_Sum_with_No_adjacent_Elements.py
+++ Dynamic programming/Maximum_subset_Sum_with_No_adjacent_Elements.py
@@ -1,3 +1,15 @@
 # Maximum suset sum with no adjacent elements 
 
 
+def rob(array):
+    maxSums = array[:]
+    if len(array) == 0:
+        return 0
+    if len(array) == 1:
+        return array[0]
+    if len(array) == 2:
+        return max(array[0], array[1])
+    if len(array) > 2:
+        for i in range(2, len(array)):
+            maxSums[i] = max(maxSums[i -1] , maxSums[i -2] + array[i])
+    return maxSums[-1]
