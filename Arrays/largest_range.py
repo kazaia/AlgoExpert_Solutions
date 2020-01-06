95a0bdff24c0661987f643700857eefa89dea2d8
kaoutar ZAIA <kaoutarZaia@gmail.com>
2020-01-06 20:22:43 +0100

    Update largest_range.py

--- Arrays/largest_range.py
+++ Arrays/largest_range.py
@@ -1,3 +1,22 @@
 # Largest range
 
 
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) ==1:
+            return 1
+        longest_count = 1
+        current_count = 1
+        nums.sort()
+        for i in range(len(nums) -1):
+            if nums[i] != nums[i+1]:
+                if nums[i] +1 == nums[i+1]:
+                    current_count += 1
+                else:
+                    longest_count = max(longest_count, current_count)
+                    current_count = 1               
+        return max(longest_count, current_count)
+            
+  
