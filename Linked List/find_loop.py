73529b8ed22decf37f0efec784b924e525d40edc
kaoutar ZAIA <kaoutarZaia@gmail.com>
2020-01-18 06:30:48 +0100

    Update find_loop.py

--- Linked List/find_loop.py
+++ Linked List/find_loop.py
@@ -1,8 +1,26 @@
 # Find loop 
 
 
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
+def findLoop(head):
+    fast = slow = head
+	fast = fast.next.next
+	slow = slow.next
+	while fast != slow:
+		fast = fast.next.next
+		slow = slow.next
+	fast = head
+	while fast != slow: 
+		fast = fast.next
+		slow = slow.next
+	return fast	
+	
 
 
 
