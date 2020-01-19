bfc0e389eacac9ffa422b132a0e681634d3d76b8
kaoutar ZAIA <kaoutarZaia@gmail.com>
2020-01-19 16:43:47 +0100

    Create single_cycle_check.py

---
+++ Graphs/single_cycle_check.py
@@ -0,0 +1,17 @@
+# Single cycle check 
+
+
+def hasSingleCycle(array):
+    numOfVisitedElements = 0
+    n = len(array)
+    currentIdx = 0
+    while numOfVisitedElements < n :
+        if numOfVisitedElements > 0 and currentIdx == 0:
+            return False
+        numOfVisitedElements += 1
+        currentIdx = getNextIdx(currentIdx, array)
+    return currentIdx == 0
+
+def getNextIdx(i, array):
+    nextIdx = (i + array[i]) % len(array)
+    return nextIdx if nextIdx >= 0  else nextIdx + len(array)
